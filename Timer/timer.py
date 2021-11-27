3e6a7aadff4fc127099846b1a0f4094938700bc2
Twelve <kana.suzucki@gmail.com>
2021-11-27 03:55:42 +0300

    added class Timer which contains function now. This function returned current time in determind format

---
+++ Timer/timer.py
@@ -0,0 +1,11 @@
+'''Contain only one class 'Timer', which execute different functions with time'''
+import time
+
+class Timer:
+    '''
+        Class 'Timer' represents package of functions to work with time
+        Class contains function 'now' to get current time
+        '''
+    def now() -> str:
+        '''Return time in format "Weekday Month Day Time Year"'''
+        return time.ctime(time.time())
